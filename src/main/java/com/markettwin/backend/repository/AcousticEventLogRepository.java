9a6b89ea68c2e270452812907edb9ae8a4db1d5a
eegun1009 <worjs55@gmail.com>
2026-07-24 00:00:14 +0900

    ERD 변경 및 변경 테이블 반영

--- src/main/java/com/markettwin/backend/repository/AcousticEventLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.markettwin.backend.repository;
-
-import com.markettwin.backend.domain.entity.AcousticEventLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcousticEventLogRepository extends JpaRepository<AcousticEventLog, Long> {
-}
--- src/main/java/com/markettwin/backend/repository/RadarReadingLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.markettwin.backend.repository;
-
-import com.markettwin.backend.domain.entity.RadarReadingLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RadarReadingLogRepository extends JpaRepository<RadarReadingLog, Long> {
-}
